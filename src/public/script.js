ca005302157123121bbd3ab7da73bdfad18c453a
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-14 21:08:57 +0700

    Add mute/unmute audio function

--- src/public/script.js
+++ src/public/script.js
@@ -15,7 +15,7 @@ let myVideoStream;
 navigator.mediaDevices
 	.getUserMedia({
 		video: true,
-		audio: false,
+		audio: true,
 	})
 	.then((stream) => {
 		myVideoStream = stream;
@@ -72,4 +72,31 @@ const addVideoStream = (video, stream) => {
 const scrollToBottom = () => {
 	let d = $(".main__chat_window");
 	d.scrollTop(d.prop("scrollHeight"));
+};
+
+const setMute = () => {
+	const enabled = myVideoStream.getAudioTracks()[0].enabled;
+	if (enabled) {
+		myVideoStream.getAudioTracks()[0].enabled = false;
+		setUnmuteButton();
+	} else {
+		setMuteButton();
+		myVideoStream.getAudioTracks()[0].enabled = true;
+	}
+};
+
+const setMuteButton = () => {
+	const html = `
+		<i class="fas fa-microphone"></i>
+		<span>Mute</span>
+	`;
+	document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+	const html = `
+		<i class="unmute fas fa-microphone-slash"></i>
+		<span>Unmute</span>
+	`;
+	document.querySelector(".main__mute_button").innerHTML = html;
 };
