c5dd2ea80804504fd85a408902e1480b87f28f86
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-14 14:39:36 +0700

    Initialize first view

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const path = require("path");
 const server = require("http").Server(app);
 const PORT = 5000;
 
 // HTTP logger
+app.use(morgan("combined"));
 
 app.set("view engine", "ejs");
-
-app.use(morgan("combined"));
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
 	res.render("room");
