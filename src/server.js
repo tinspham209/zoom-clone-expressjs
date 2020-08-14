1d789aa660d8e992175a0af1b0d4e9101e34bc6a
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-14 14:34:56 +0700

    import room UI to app

--- src/server.js
+++ src/server.js
@@ -5,10 +5,13 @@ const server = require("http").Server(app);
 const PORT = 5000;
 
 // HTTP logger
+
+app.set("view engine", "ejs");
+
 app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
-	res.status(200).send("Hello world");
+	res.render("room");
 });
 
 server.listen(PORT, () =>
