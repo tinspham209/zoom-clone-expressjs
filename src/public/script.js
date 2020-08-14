23a1823e99de47f8735594012070cf2db7154cad
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-14 15:18:37 +0700

    import socket to FE

--- src/public/script.js
+++ src/public/script.js
@@ -1,3 +1,5 @@
+const socket = io("/");
+
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
@@ -13,6 +15,8 @@ navigator.mediaDevices
 		myVideoStream = stream;
 		addVideoStream(myVideo, stream);
 	});
+
+socket.emit("join-room");
 
 const addVideoStream = (video, stream) => {
 	video.srcObject = stream;
